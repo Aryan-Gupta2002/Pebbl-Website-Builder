02b9111328b73d4a69426e8e7e238f9e39d33df6
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-19 23:29:34 +0530

    prefetching Client

--- pebbl/src/app/page.tsx
+++ pebbl/src/app/page.tsx
@@ -1,6 +1,17 @@
-import { caller } from "@/trpc/server";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "@/app/client";
+import { Suspense } from "react";
 const Page = async () => {
-  
-  return <div>{JSON.stringify(data)}</div>;
+  const queryClient = getQueryClient();
+  // getQueryClient creates a query cache container (a bucket to store fetched data temporarily on the server).
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "Aryan" }));
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Client />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 export default Page;
