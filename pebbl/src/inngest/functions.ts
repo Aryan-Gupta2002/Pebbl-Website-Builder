7bf629f9ad6f4a7b39d9623e4678034e0d654766
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-20 15:05:35 +0530

    Coding Agent using Nvidia NIM

--- pebbl/src/inngest/functions.ts
+++ pebbl/src/inngest/functions.ts
@@ -5,17 +5,18 @@ import { createAgent, openai } from "@inngest/agent-kit";
 export const processTask = inngest.createFunction(
   { id: "process-task2", triggers: { event: "app/task.created" } },
   async ({ event }) => {
-    const summarizer = createAgent({
-      name: "summarizer",
-      system: "You are an expert summarizer. You summarize in 3 words",
+    const codeAgent = createAgent({
+      name: "Code Agent",
+      system:
+        "You are an expert next.js developer. You write readable,maintanable code. You write simple Next.js & React snippets.",
       model: openai({
         model: "meta/llama-3.1-8b-instruct",
         apiKey: process.env.NIM_API_KEY,
         baseUrl: process.env.BASE_URL,
       }),
     });
-    const { output } = await summarizer.run(
-      `Summarize the following text: ${event.data.value}`,
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
     );
     return { output };
   },
