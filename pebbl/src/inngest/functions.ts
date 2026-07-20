68c1fb6aaf139ee304378813f8a938b17eed4baf
Aryan-Gupta2002 <ag912388@gmail.com>
2026-07-20 14:55:19 +0530

    Summarizer agent using NIM API

--- pebbl/src/inngest/functions.ts
+++ pebbl/src/inngest/functions.ts
@@ -1,12 +1,22 @@
+import { success } from "zod";
 import { inngest } from "./client";
+import { createAgent, openai } from "@inngest/agent-kit";
 
 export const processTask = inngest.createFunction(
   { id: "process-task2", triggers: { event: "app/task.created" } },
-  async ({ event, step }) => {
-    const result = await step.run("handle-task", async () => {
-      return { processed: true, id: event.data.id };
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "summarizer",
+      system: "You are an expert summarizer. You summarize in 3 words",
+      model: openai({
+        model: "meta/llama-3.1-8b-instruct",
+        apiKey: process.env.NIM_API_KEY,
+        baseUrl: process.env.BASE_URL,
+      }),
     });
-    await step.sleep("pause", "5s");
-    return { message: `Task ${event.data.value} complete`, result };
+    const { output } = await summarizer.run(
+      `Summarize the following text: ${event.data.value}`,
+    );
+    return { output };
   },
 );
