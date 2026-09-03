2be3eb6642724593adda7475eb0302ae159fd94b
Aryan-Gupta2002 <ag912388@gmail.com>
2026-09-03 21:14:22 +0530

    Updated Themes and Procedures

---
+++ pebbl/src/hooks/use-current-theme.ts
@@ -0,0 +1,12 @@
+// This file and code is not needed anymore
+// Just leaving here for memory
+
+import { useTheme } from "next-themes";
+
+export const useCurrentTheme = () => {
+  const { theme, systemTheme } = useTheme();
+  if (theme === "dark" || theme === "light") {
+    return theme;
+  }
+  return systemTheme;
+};
