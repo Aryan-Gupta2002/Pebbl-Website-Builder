1e8158c4e8406ba29080a1b1e544ccbce9525582
Aryan-Gupta2002 <ag912388@gmail.com>
2026-09-04 13:09:57 +0530

    Authentication

--- pebbl/src/modules/home/ui/components/project-form.tsx
+++ pebbl/src/modules/home/ui/components/project-form.tsx
@@ -14,7 +14,7 @@ import { Button } from "@/components/ui/button";
 import { Form, FormField } from "@/components/ui/form";
 import { useRouter } from "next/navigation";
 import { PROJECT_TEMPLATES } from "../../constants";
-import { template } from "../../../../../template";
+import { useClerk } from "@clerk/nextjs";
 
 const formSchema = z.object({
   value: z
@@ -25,6 +25,7 @@ const formSchema = z.object({
 
 export const ProjectForm = () => {
   const router = useRouter();
+  const clerk = useClerk();
   const trpc = useTRPC();
   const queryClient = useQueryClient();
   const form = useForm<z.infer<typeof formSchema>>({
@@ -42,8 +43,11 @@ export const ProjectForm = () => {
         // TODO: Invalidate usage status
       },
       onError: (error) => {
-        // TODO: Redirect to pricing page if specific error
         toast.error(error.message);
+        if (error.data?.code === "UNAUTHORIZED") {
+          clerk.openSignIn();
+        }
+        // TODO: Redirect to pricing page if specific error
       },
     }),
   );
